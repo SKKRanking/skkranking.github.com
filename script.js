d0946a30c0f893fde7c0638eb95316dd38b5e478
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-06-23 00:48:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
+let url = "https://divine-morning-31b8.skk-kaos-ranking.workers.dev/api/getClimberData";
 
-var pattern = "https://www.sostokt.com/api/climbs/0a7be0ac-4ff2-4bf2-bce3-2f7021a82def/latest-sends";
-
-function redirect(requestDetails) {
-  console.log("Redirecting: " + requestDetails.url);
-  return {
-    redirectUrl: "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif"
-  };
-}
-
-browser.webRequest.onBeforeRequest.addListener(
-  redirect,
-  {urls:[pattern]},
-  ["blocking"]
-);
+$.getJSON(url, function(data) {
+  console.log(data);
+});
