56f28b3a8a10ea0f53e27348e068d0c46ec938bc
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-06-23 01:41:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let url = "https://divine-morning-31b8.skk-kaos-ranking.workers.dev/api/getClimberData";
 
-$.getJSON(url, function(data) {
-  console.log(data);
-});
+
+$(document).ready(() => {
+  $.getJSON(url, function(data) {
+    for(let climber in data) {
+      let stats = data[climber].split("-");
+      
+      let totalPoints = 0;
+      
+      let gradeIndex = 0;
+      for(let gradeStat of stats) {
+        let flashes = parseInt(gradeStat.substr(0,2));
+        let sends = parseInt(gradeStat.substr(3,6));
+        
+        let gradePoints =  50 * sends * (2 * gradeIndex + 3);
+        
+        totalPoints += gradePoints;
+            
+        gradeIndex++;
+      }
+      
+      console.log(climber, totalPoints);
+    }
+  });
+})
