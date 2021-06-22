6373667e7567e9a1473cdbdd4e1a2b47246c5a13
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-06-23 01:51:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ $(document).ready(() => {
       
       console.log(climber);
       for(let gradeStat of stats) {
-        let flashes = parseInt(gradeStat.substr(0,2));
-        let sends = parseInt(gradeStat.substr(3,6));
+        let flashes = parseInt(gradeStat.substring(0,2));
+        let sends = parseInt(gradeStat.substring(3,6));
         
         console.log(flashes,sends);
         
