4062e2c672e22b50059f8e4e7dace26fcf8184dc
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-06-23 02:51:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let url = "https://divine-morning-31b8.skk-kaos-ranking.workers.dev/api/getClimberData";
 
+let climbers = [];
 
 $(document).ready(() => {
   $.getJSON(url, function(data) {
@@ -28,7 +29,11 @@ $(document).ready(() => {
         gradeIndex++;
       }
       
-      $("tbody").after('<tr><th scope="row">1</th><td>' + climber.split(" ")[0] + '</td><td>' + climber.split(" ")[1] + '</td><td>' + totalPoints + '</td></tr>');
+      climbers.push({"first": climber.split(" ")[0], "second": climber.split(" ")[1], "points": totalPoints};
     }
+    
+    climbers.sort((climber1, climber2) => {
+      return climber1.points > climber2.points;
+    })
   });
 })
