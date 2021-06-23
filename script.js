1797a33a77237ee5fad29fe077953ee193d81f02
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-06-23 02:54:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,19 @@ let climbers = [];
 
 $(document).ready(() => {
   $.getJSON(url, function(data) {
-    console.log(data);
     
     for(let climber in data) {
       let stats = data[climber].split("-");
       
-      console.log(stats);
       
       let totalPoints = 0;
       
       let gradeIndex = 0;
       
-      console.log(climber);
       for(let gradeStat of stats) {
         let sends = parseInt(gradeStat.substring(0,3));
         let flashes = parseInt(gradeStat.substring(3));
         
-        console.log(sends,flashes);
         
         let gradePoints =  50 * sends * (gradeIndex + 1) + 50 * flashes * (gradeIndex+2);
         
