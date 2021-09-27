2dc0f4473098b08421c207e6fc44846cd07b8827
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-09-27 22:52:40 +0200

    Update script.js

--- lists/script.js
+++ lists/script.js
@@ -1,11 +1,12 @@
 $.getJSON( "ajax/test.json", function(data) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  let listPicker = $('#list-tab');
+  let listViewer = $(#'nav-tabContent');
+                     
+  $.each(data, function(key, val) {
+    let lower = val.name.toLowerCase();
+    
+    listPicker.append('<a class="list-group-item list-group-item-action active" id="list-' + lower '-list" data-bs-toggle="list" href="#list-' + lower + '" role="tab" aria-controls="list-' + lower + '">val.name</a>');
+    
+    listViewer.append('<div class="tab-pane fade show active" id="list-' + lower + '" role="tabpanel" aria-labelledby="list-' + classics + '-list">' + val.problems[0].name + '</div>');
   });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
 });
