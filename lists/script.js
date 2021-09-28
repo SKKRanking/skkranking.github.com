419553270e5199c23c9392e94f1ab68b103e13e5
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-09-28 12:57:11 +0200

    Update script.js

--- lists/script.js
+++ lists/script.js
@@ -1,3 +1,25 @@
+let paletteMap = {
+  "4": "ffee2f",
+  "4+": "ffd42a",
+  "5": "ffb925",
+  "5+": "ff9f1f",
+  "6A": "ff841a",
+  "6A+": "ff6a15",
+  "6B": "ff4f10",
+  "6B+": "ff350a",
+  "6C": "ff1a05",
+  "6C+": "ff0000",
+  "7A": "fc0114",
+  "7A+": "f90229",
+  "7B": "f6033d",
+  "7B+": "f30451",
+  "7C": "ef0566",
+  "7C+": "ec067a",
+  "8A": "e9078e",
+  "8A+": "e608a3",
+  "8B": "e309b7"
+}
+
 $.getJSON("lists.json", function(data) {
   let listPicker = $('#list-tab');
   let listViewer = $('#nav-tabContent');
@@ -18,7 +40,7 @@ $.getJSON("lists.json", function(data) {
     
     for(problem of val.problems) {
       listGroup
-        .append('<li class="list-group-item d-flex justify-content-between align-items-center">' + problem.name + '<span><small>' + problem.setter + ' </small><span class="badge bg-info rounded-pill">' + problem.grade + '</span></span></li>');
+        .append('<li class="list-group-item d-flex justify-content-between align-items-center">' + problem.name + '<span><small>' + problem.setter + ' </small><span class="badge rounded-pill" style="background-color: #' + paletteMap[problem.grade] + ';">' + problem.grade + '</span></span></li>');
     }
   });
 });
