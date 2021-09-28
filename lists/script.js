e2f3c2ee3bd99aa0941930acf14e356fa6fcaee9
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-09-28 08:21:48 +0200

    Update script.js

--- lists/script.js
+++ lists/script.js
@@ -5,7 +5,7 @@ $.getJSON("lists.json", function(data) {
   $.each(data, function(key, val) {
     let lower = val.name.toLowerCase();
     
-    listPicker.append('<a class="list-group-item list-group-item-action active" id="list-' + lower '-list" data-bs-toggle="list" href="#list-' + lower + '" role="tab" aria-controls="list-' + lower + '">val.name</a>');
+    listPicker.append('<a class="list-group-item list-group-item-action active" id="list-' + lower '-list" data-bs-toggle="list" href="#list-' + lower + '" role="tab" aria-controls="list-' + lower + '">' + val.name + '</a>');
     
     listViewer.append('<div class="tab-pane fade show active" id="list-' + lower + '" role="tabpanel" aria-labelledby="list-' + classics + '-list">' + val.problems[0].name + '</div>');
   });
