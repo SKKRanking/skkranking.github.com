b4f34ae47f6a8a3e9842c8e304508eb7b73900d6
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-09-28 11:18:23 +0200

    Update script.js

--- lists/script.js
+++ lists/script.js
@@ -5,15 +5,8 @@ $.getJSON("lists.json", function(data) {
   $.each(data, function(key, val) {
     let lower = val.name.toLowerCase();
     
-    let triggerEl = listPicker
+    listPicker
       .append('<a class="list-group-item list-group-item-action" id="list-' + lower + '-list" data-bs-toggle="list" href="#list-' + lower + '" role="tab" aria-controls="list-' + lower + '">' + val.name + '</a>');
-    
-    var tabTrigger = new bootstrap.Tab(triggerEl)
-
-    triggerEl.addEventListener('click', function (event) {
-      event.preventDefault()
-      tabTrigger.show()
-   })
     
     listViewer.append('<div class="tab-pane fade" id="list-' + lower + '" role="tabpanel" aria-labelledby="list-' + lower + '-list">' + val.problems[0].name + '</div>');
   });
