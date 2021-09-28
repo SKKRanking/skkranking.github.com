c9656f4db654d54a6e3bf7df04954d18fce17c0e
SKKRanking <85957830+SKKRanking@users.noreply.github.com>
2021-09-28 11:28:58 +0200

    Update script.js

--- lists/script.js
+++ lists/script.js
@@ -9,12 +9,12 @@ $.getJSON("lists.json", function(data) {
       .append('<a class="list-group-item list-group-item-action" id="list-' + lower + '-list" data-bs-toggle="list" href="#list-' + lower + '" role="tab" aria-controls="list-' + lower + '">' + val.name + '</a>');
     
     let listGroup = listViewer
-      .append('<div class="tab-pane fade" id="list-' + lower + '" role="tabpanel" aria-labelledby="list-' + lower + '-list">' + val.problems[0].name + '</div>')
+      .append('<div class="tab-pane fade" id="list-' + lower + '" role="tabpanel" aria-labelledby="list-' + lower + '-list"></div>')
       .append('<ul class="list-group"></ul>');
     
     for(problem of val.problems) {
       listGroup
-        .append('<li class="list-group-item">' + problem.name + ' by ' + problem.setter + ' <span class="badge bg-info">' + problem.gradew + '</span></li>');
+        .append('<li class="list-group-item">' + problem.name + ' by ' + problem.setter + ' <span class="badge bg-info">' + problem.grade + '</span></li>');
     }
   });
 });
